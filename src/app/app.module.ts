9225b9b52d0820682e8d415ebe010dd5e466a23d
thisMiladnafar <milarnafar78@gmail.com>
2021-11-12 21:50:14 +0330

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import {MonacoEditorModule} from "ngx-monaco-editor";
+import { CodeEditorComponent } from './component/code-editor/code-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MonacoEditorModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
