3c864438daf88c54420a9c6679a92b8cc8ede0bb
m.nafar <m.nafar@mohaymen.ir>
2021-11-14 08:47:21 +0330

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,21 @@ import {CodeEditorLanguageType, CodeEditorOptions, CodeEditorTheme} from "./comp
 })
 export class AppComponent {
   title = 'monaco';
+  typescriptCode = `
+import banana
 
+class Monkey:
+    # Bananas the monkey can eat.
+    capacity = 10
+    def eat(self, n):
+        """Make the monkey eat n bananas!"""
+        self.capacity -= n * banana.size
+
+    def feeding_frenzy(self):
+        self.eat(9.25)
+        return "Yum yum"
+
+  `;
 
   codeEditorOption:CodeEditorOptions={
     language:CodeEditorLanguageType.python,
--- src/app/component/code-editor/code-editor.component.ts
+++ src/app/component/code-editor/code-editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {MonacoEditorConstructionOptions, MonacoEditorLoaderService} from "@materia-ui/ngx-monaco-editor";
 import {CodeEditorOptions} from "./code-editor-configs";
 import {filter, take} from "rxjs/operators";
@@ -8,37 +8,24 @@ import {filter, take} from "rxjs/operators";
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.scss']
 })
-export class CodeEditorComponent {
+export class CodeEditorComponent implements OnChanges{
   // @ViewChild('ID', {static: true}) NAME: ElementRef | undefined;
 
   @Input() public options?: CodeEditorOptions;
+  @Input() public value?: string="";
 
-  typescriptCode = `
-  import banana
 
-class Monkey:
-    # Bananas the monkey can eat.
-    capacity = 10
-    def eat(self, n):
-        """Make the monkey eat n bananas!"""
-        self.capacity -= n * banana.size
-
-    def feeding_frenzy(self):
-        self.eat(9.25)
-        return "Yum yum"
-
-  `;
   defaultOptions: MonacoEditorConstructionOptions = {
     theme: 'vs',
     readOnly: false, minimap: {enabled: false},
-    value: this.typescriptCode,
+    value: this.value,
+    language:"python"
   };
   modelUri?: monaco.Uri;
 
 
   constructor(private monacoLoader: MonacoEditorLoaderService) {
-
-
+    this.registerJSONValidationSchema();
   }
 
   mergeOptions(moreOptions?: any) {
@@ -49,5 +36,78 @@ class Monkey:
     }
   }
 
+   createDependencyProposals(range:any) {
+    // returning a static list of proposals, not even looking at the prefix (filtering is done by the Monaco editor),
+    // here you could do a server side lookup
+    return [
+      {
+        label: '"lodash"',
+        kind: monaco.languages.CompletionItemKind.Function,
+        documentation: 'The Lodash library exported as Node.js modules.',
+        insertText: '"lodash": "*"',
+        range: range
+      },
+      {
+        label: '"express"',
+        kind: monaco.languages.CompletionItemKind.Function,
+        documentation: 'Fast, unopinionated, minimalist web framework',
+        insertText: '"express": "*"',
+        range: range
+      },
+      {
+        label: '"mkdirp"',
+        kind: monaco.languages.CompletionItemKind.Function,
+        documentation: 'Recursively mkdir, like <code>mkdir -p</code>',
+        insertText: '"mkdirp": "*"',
+        range: range
+      },
+      {
+        label: '"my-third-party-library"',
+        kind: monaco.languages.CompletionItemKind.Function,
+        documentation: 'Describe your library here',
+        insertText: '"${1:my-third-party-library}": "${2:1.2.3}"',
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        range: range
+      }
+    ];
+  }
 
+
+
+
+
+  private async registerJSONValidationSchema() {
+    await this.monacoLoader.isMonacoLoaded$.pipe(filter(isLoaded => isLoaded), take(1)).toPromise();
+    monaco.languages.registerCompletionItemProvider('python', {
+      provideCompletionItems:  (model, position) =>{
+        // find out if we are completing a property in the 'dependencies' object.
+        var textUntilPosition = model.getValueInRange({
+          startLineNumber: 1,
+          startColumn: 1,
+          endLineNumber: position.lineNumber,
+          endColumn: position.column
+        });
+        // var match = textUntilPosition.match(
+        //   /"dependencies"\s*:\s*\{\s*("[^"]*"\s*:\s*"[^"]*"\s*,\s*)*([^"]*)?$/
+        // );
+        // if (!match) {
+        //   return { suggestions: [] };
+        // }
+        var word = model.getWordUntilPosition(position);
+        var range = {
+          startLineNumber: position.lineNumber,
+          endLineNumber: position.lineNumber,
+          startColumn: word.startColumn,
+          endColumn: word.endColumn
+        };
+        return {
+          suggestions: this.createDependencyProposals(range)
+        };
+      }
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
 }
